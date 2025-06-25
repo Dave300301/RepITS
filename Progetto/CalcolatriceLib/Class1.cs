ac754dc5cfff7e755172a7e377039927a6213d24
Dave300301 <davide.castelli.14-2@accademiadigitaleliguria.it>
2025-06-25 15:32:32 +0200

    #1 update Calcolatrice

---
+++ Progetto/CalcolatriceLib/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace CalcolatriceLib
+{
+    public class Calcolatrice
+    {
+        public Calcolatrice()
+        {
+        }
+        public int Somma(int a, int b)
+        {
+            return a+b;
+        }
+    }
+}
---
+++ Progetto/TestCalcolatrice/Test1.cs
@@ -0,0 +1,21 @@
+﻿namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CalcolatriceLib.Calcolatrice calcolatrice = new CalcolatriceLib.Calcolatrice();
+            double risultato = calcolatrice.Somma(2, 3);
+            Assert.AreEqual(5, risultato, 0.0001, "La somma di 2 e 3 dovrebbe essere 5");
+        }
+        [TestMethod]
+        public void TestSommaCon0()
+        {
+            CalcolatriceLib.Calcolatrice calcolatrice = new CalcolatriceLib.Calcolatrice();
+            double risultato = calcolatrice.Somma(0, 0);
+            Assert.AreEqual(0, risultato, 0.0001, "La somma di 0 e 0 dovrebbe essere 0");
+        }
+    }
+}
